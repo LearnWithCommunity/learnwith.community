2390d7aec35633dbcc73b0fb7ff981217258de83
Vasanth Developer <vsnthdev@gmail.com>
2022-01-10 20:34:08 +0530

    🔨 added tailwindcss for styling

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,16 +4,27 @@
  */
 
 import { Config } from '@stencil/core'
+import { postcss } from '@stencil/postcss'
+import tailwindcss from 'tailwindcss'
 
 export const config: Config = {
-    taskQueue: 'async',
-    minifyCss: true,
     minifyJs: true,
+    minifyCss: true,
+    taskQueue: 'async',
+    globalStyle: 'src/assets/styles/index.css',
+    devServer: {
+        reloadStrategy: 'pageReload'
+    },
     outputTargets: [
         {
             type: 'www',
-            serviceWorker: null,
+            serviceWorker: false,
             baseUrl: 'https://learnwith.community',
         }
+    ],
+    plugins: [
+        postcss({
+            plugins: [tailwindcss]
+        })
     ]
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/*
+ *  TailwindCSS config file for learnwith.community project.
+ *  Created On 10 January 2022
+ */
+
+module.exports = {
+    darkMode: 'class',
+    content: [
+        './src/components/**/*.tsx',
+        './src/index.html'
+    ],
+    theme: {},
+    plugins: []
+}
