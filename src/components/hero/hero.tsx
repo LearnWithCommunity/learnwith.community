b7d08586ea44b93b3bbaed01805058795fdb631b
Vasanth Developer <vsnthdev@gmail.com>
2022-01-12 13:03:11 +0530

    🐛 removed center offset of hero section content

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -12,7 +12,7 @@ import typography from './../../assets/img/typography.svg';
 export class Hero {
     render() {
         return <main class="py-7 px-7 h-full flex flex-col justify-center items-center text-[#3C2A31]">
-            <div class="container flex flex-col mb-24 space-y-6 text-center">
+            <div class="container flex flex-col space-y-6 text-center">
                 <div class="text-[#8D29DC] h-6 md:h-7" innerHTML={typography}></div>
                 <h1 class="mx-auto font-bold text-5xl leading-tight md:leading-snug md:text-6xl md:max-w-2xl lg:text-7xl lg:max-w-5xl lg:leading-normal dark:text-white">Let's learn and grow with the community.</h1>
             </div>
