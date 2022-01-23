19c0d8aefd59333bce1d117959d0f9b2c5ca8175
Vasanth Developer <vsnthdev@gmail.com>
2022-01-23 21:39:09 +0530

    🐛 fixed hero text is misaligned on smaller screens

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -13,12 +13,12 @@ import heroSVG from './hero.svg';
 export class Hero {
     render() {
         return <main class="relative h-full flex flex-col justify-center items-center text-[#3C2A31]">
-            <div class="absolute px-7 container flex flex-col space-y-6 text-center -mt-48 md:mb-0">
+            <div class="absolute px-7 container flex flex-col space-y-6 text-center md:mb-0 xl:mb-20">
                 <div class="text-[#8D29DC] h-6 md:h-7" innerHTML={typography}></div>
                 <h1 class="mx-auto font-bold text-5xl leading-tight transition-colors md:leading-snug md:text-6xl md:max-w-2xl lg:text-7xl lg:max-w-5xl lg:leading-normal dark:text-white">Let's learn and grow with the community.</h1>
             </div>
             <div class="absolute bottom-0 w-screen overflow-hidden pointer-events-none">
-                <div class="w-[140rem] -ml-[80rem] md:w-[auto] md:-ml-[30rem]" innerHTML={heroSVG}></div>
+                <div class="w-[140rem] -ml-[80rem] md:w-[auto] md:-ml-[30rem] lg:mx-0" innerHTML={heroSVG}></div>
             </div>
         </main>
     }
